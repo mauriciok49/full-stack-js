be92fb757f30d73335505e0a2c73b8a67f2a7c8a
mauriciok49 <mauriciocerqueirak49@gmail.com>
2025-10-09 15:58:29 -0300

    atualizei app.js

--- 02. Control FLow/app.js
+++ 02. Control FLow/app.js
@@ -1,7 +1,9 @@
-for (let i = 1; i <= 10; i++) {
-    console.log('laco externo', i)
-    for (let j = 1; j <= 5; j++) {
-        console.log('laco interno', j)
-        
-    }   
-}
+const singers = ["Freddie Mercury", "Adele", "Elvis Presley"]
+const actors = ["Leonardo DiCaprio", "Tom Hanks", "Scarlett Johansson"]
+const players = ["Pelé", "Lionel Messi", "Cristiano Ronaldo"]
+
+const allArrays = [singers, actors, players]
+
+
+console.log(allArrays[2][1])
+
