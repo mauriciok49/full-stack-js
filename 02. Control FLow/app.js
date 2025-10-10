99a9e36b666daf8a85a44654bc7c2e38bf94b772
mauriciok49 <mauriciocerqueirak49@gmail.com>
2025-10-10 09:28:59 -0300

    atualizei o app.js

--- 02. Control FLow/app.js
+++ 02. Control FLow/app.js
@@ -1,9 +1,6 @@
-const singers = ["Freddie Mercury", "Adele", "Elvis Presley"]
-const actors = ["Leonardo DiCaprio", "Tom Hanks", "Scarlett Johansson"]
-const players = ["Pelé", "Lionel Messi", "Cristiano Ronaldo"]
+function digaOla (nome) {
+    console.log(`ola ${nome}`);
+    
+}
 
-const allArrays = [singers, actors, players]
-
-
-console.log(allArrays[2][1])
-
+digaOla("Mauricio")
